1cf3f41f53cc549b4adfb595fddad8f674eea4e1
Jennifer Wong <jennifer.design@yahoo.com>
2020-09-08 20:50:09 -0500

    Found solution to render output for view all employees while renaming object keys to look better visually

--- server.js
+++ server.js
@@ -39,24 +39,24 @@ connection.connect(function(err) {
       // console.log(choice); output: View employees
       viewEmployee();
     }
-    if (answer === "View departments") {
-      viewDepart();
-    }
-    if (answer === "View roles") {
-      viewRoles();
-    }
-    if (answer === "Add departments") {
-      addDepart();
-    }
-    if (answer === "Add roles") {
-      addRoles();
-    }
-    if (answer === "Add employees") {
-      addEmployee();
-    }
-    if (answer === "Update employee role") {
-      updateEmployeeRole();
-    }
+    // if (answer === "View departments") {
+    //   viewDepart();
+    // }
+    // if (answer === "View roles") {
+    //   viewRoles();
+    // }
+    // if (answer === "Add departments") {
+    //   addDepart();
+    // }
+    // if (answer === "Add roles") {
+    //   addRoles();
+    // }
+    // if (answer === "Add employees") {
+    //   addEmployee();
+    // }
+    // if (answer === "Update employee role") {
+    //   updateEmployeeRole();
+    // }
   });
 }
 
@@ -65,30 +65,48 @@ let viewEmployee = () => {
     "SELECT employee.id, first_name, last_name, title, department.name, role.salary FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id",
     function(err, res) {
       if (err) throw err;
-      console.log(res);
-      start();
+
+      let adjRes = res.map(obj => {
+        let rObj = { ...obj }
+        rObj["ID"] = obj["id"];
+        rObj["First Name"] = obj["first_name"];
+        rObj["Last Name"] = obj["last_name"];
+        rObj["Title"] = obj["title"];
+        rObj["Department"] = obj["name"];
+        rObj["Salary"] = obj["salary"];
+        delete rObj["id"];
+        delete rObj["first_name"];
+        delete rObj["last_name"];
+        delete rObj["title"];
+        delete rObj["name"];
+        delete rObj["salary"];
+        return rObj
+      })
+      console.table(adjRes);
+
+      //start();
     }
   )
 };
 
-let viewDepart = () => {
-  connection.query(
-    "QUERY", {},
-    function(err, res) {
-      if (err) throw err;
-      console.log("testing2")
-      start();
-    }
-  )
-};
-
- let viewRoles = () => {
-   connection.query(
-     "QUERY", {},
-     function(err, res) {
-       if (err) throw err;
-       console.log("testing3")
-       start();
-     }
-   )
- };
+// let viewDepart = () => {
+//   connection.query(
+//     "QUERY", {},
+//     function(err, res) {
+//       if (err) throw err;
+//       console.log("testing2")
+//       start();
+//     }
+//   )
+// };
+//
+//  let viewRoles = () => {
+//    connection.query(
+//      "QUERY", {},
+//      function(err, res) {
+//        if (err) throw err;
+//        console.log("testing3")
+//        start();
+//      }
+//    )
+//  };
